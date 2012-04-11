5df40b77bdcc33647aca7e685c1cd63fa7607b29
Denis Laliberté <denis.laliberte@gmail.com>
2012-04-11 15:58:04 -0400

    objet jeu, a terminer

--- bloc.js
+++ bloc.js
@@ -1,10 +1,9 @@
 
 /* ********** ********** ********** ********** ********** ********** ********** */
-				//objet bloc
+				//objet bloc et jeu
 /* ********** ********** ********** ********** ********** ********** ********** */
 
-var POSITION_DEPART =Math.round( (NOMBRE_COLONNE/2)*DIMENSION_BLOC);
-
+var POSITION_DEPART =460;
 var BLEU = 0;
 var VERT = 1;
 var ROUGE = 2;
@@ -14,10 +13,53 @@ tableauStyle[0] = "bleu";
 tableauStyle[1] = "vert";
 tableauStyle[2] = "rouge";
 tableauStyle[3] = "jaune";
-function Bloc(id){
+var DIMENSION_BLOC = 40; //dimension largeur et hauteur d'un bloc, en px 
+var NOMBRE_BLOC_LIGNE = 3; //nombre de bloc pour faire une ligne valide
+var LIMITTE_GAUCHE =100;
+var LIMITTE_DROITE =900;
+var NOMBRE_LIGNE_MAX = 14;
+var PLANCHER = 580;
+var NOMBRE_COLONNE = 10;
+
+function Jeu(){
+    var idBloc = 0;
+    var blocActif;
+    var tableauPlancher = new Array();
+    var tableauBloc = new Array(10,10);
+    // TODO: a tester plus tard, je ne suis pas sur s'il faut initialiser un array
+    //mais ça bugais avant que je le fasse
+    for (var i = 0;i<NOMBRE_LIGNE_MAX+1;i++){
+        tableauBloc[i]=new Array();
+    	for (var j = 0;j<NOMBRE_COLONNE;j++){
+    		tableauBloc[i][j]=0;
+	}
+    }
+    for (var i=0;i<NOMBRE_COLONNE;i++){
+		tableauPlancher[i]=0;
+	}
+    function nouveauBloc{
+        blocActif = new bloc(idBloc,tableauBloc);
+        idBloc++;
+        return bloc;
+    }
+    function ajoutBloc(){
+        blocActif.ligne = tableauPlancher[blocActif.colonne];
+        tableauBloc[blocActif.ligne][blocActif.colonne] = blocActif;
+    }
+    function verifierLigne(){
+        for(var i=0;i<10;i++){
+	    for(var j;j<10;j++){
+	        var retour = tableauBloc[i][j].validerLigne(0);	
+		//TODO : faire la même choses pour les colonnes
+	    }
+	}
+}
+
+function Bloc(id,tableau){
 	//initialisation
 	this.actif=true;
 	this.id=id;
+        this.tableau=tableau;
 	this.idCSS="bloc"+id;
 	this.y = 10;
 	this.nouveauX = 0;
@@ -25,7 +67,12 @@ function Bloc(id){
 	this.couleur = Math.round((Math.random() * 3) );
         this.style = tableauStyle[this.couleur];
 	this.colonne = NOMBRE_COLONNE/2;
-	console.log("initialisation du bloc : "+id
+        this.ligne = 0;
+        this.direction=0;
+        this.rotation = false;
+        this.acceleration = false;
+// debug :
+        console.log("initialisation du bloc : "+id
 			+"couleur : "+this.couleur
 			+"style : " +this.style);
 	//creer le div et en garder une référence
@@ -34,32 +81,35 @@ function Bloc(id){
 				+this.style+"' ></div>") );
 	this.div = document.getElementById(this.idCSS);
         this.xCourant =parseInt(this.div.offsetLeft) + POSITION_DEPART;
-	console.log("Bloc()initialisation POSITION_DEPART"+POSITION_DEPART);
+//debug	
+        console.log("Bloc()initialisation POSITION_DEPART"+POSITION_DEPART);
 	console.log("Bloc() initialisation this.div.offsetLeft"+parseInt(this.div.offsetLeft));
 	console.log("Bloc() initialisation this.xCourant : " + this.xCourant );
 	this.div.style.left = this.xCourant + "px";
 
 	console.log("Bloc() initialisation this.div.offsetLeft"+parseInt(this.div.offsetLeft));
 
-	this.tic = function() {
-	//fonction qui gere le rythme de l'annimation du bloc
+    this.tic = function() {
+    //fonction qui gere le rythme de l'annimation du bloc
 		
-		if(direction!=0){		
-		//deplacement horizontal
-			this.xCourant = this.div.offsetLeft;
-			this.nouveauX = this.xCourant +( DIMENSION_BLOC * direction);
+    	if(direction!=0){		
+    	//deplacement horizontal
+	    this.xCourant = this.div.offsetLeft;
+            this.nouveauX = this.xCourant +( DIMENSION_BLOC * direction);
 			
-			if(this.nouveauX > LIMITTE_GAUCHE 
-				&& this.nouveauX < LIMITTE_DROITE){
-				this.div.style.left = this.nouveauX + "px";
-				this.colonne +=direction;
-				console.log("bloc deplacement this.colonne"+this.colonne);
+	    if(this.nouveauX > LIMITTE_GAUCHE 
+		&& this.nouveauX < LIMITTE_DROITE){
+		    this.div.style.left = this.nouveauX + "px";
+		    this.colonne +=direction;
+//debug :
+                    console.log("bloc deplacement this.colonne"+this.colonne);
 			}
 		}
-		//rotation du bloc
-		if(rotation){
-			console.log("rotation");
-			rotation = false ;
+	//rotation du bloc
+	    if(rotation){
+		    //TODO : rotation
+                    console.log("rotation");
+		    rotation = false ;
 		}
 		if(!acceleration){
 			this.vitesse=1;
@@ -67,11 +117,9 @@ function Bloc(id){
 		else {
 			this.vitesse=5;
 		}
-		
 		//deplacement vertical
 		this.y+=this.vitesse;
 		this.div.style.top = this.y + "px";	
-		
 		//validation arret
 		
 		//calcul du plancher de la colonne que le bloc descend
@@ -87,4 +135,35 @@ function Bloc(id){
 		}
 		
 	}
+    function validerLigne(compteur){
+    /*valide pour un bloc si les suivants sur la lignes sont de la même couleur
+    s'il y en as plus de 3 en ligne ils sont tous retiré. la fonction retourne le
+    nombre de bloc à retirer*/
+	var suivante = this.colone+1;
+	if(this.couleur == this.tableau[this.ligne][suivante].couleur) {
+	//si le bloc suivant est de la même couleur on avance et on compte
+	    compteur++;
+            var nombreARetirer = this.tableau[this.ligne][suivante].validerLigne(compteur);
+	}
+	else if(compteur < NOMBRE_BLOC_LIGNE ){
+	//si le bloc suivant n'est pas de la même couleur et qu'on en as compté
+	//moins de 3 on retourne 0 bloc à retirer 
+		return 0;
+	}
+	else{
+	//si on as compté plus de trois bloc de même couleur on incrémente le compteur	
+	//et on l'assigne aux blos à retirer
+		nombreARetirer = compteur++;
+	}
+	if(nombreARetirer > 0){
+		//on retire le div du html et l'objet du tableau
+		$("#"+id).remove();
+		tableauBloc[ligne].splice(colonne,1);
+		nombreARetirer--;
+		return nombreARetirer;
+	}
+	else{
+	 return 0;
+	}
+}
 }
--- tetris.js
+++ tetris.js
@@ -1,7 +1,6 @@
 /* ********** ********** ********** ********** ********** ********** ********** 
 			imitation du jeu tetris en javascript
 /* ********** ********** ********** ********** ********** ********** ********** */
-
 /*
 todo :
 - faire un gros div game over
@@ -15,60 +14,23 @@ todo :
 -clean up commentaire et console.log
 */
 
-//constantes
-var TOUCHE_GAUCHE = 37;
-var TOUCHE_DROITE = 39;
-var TOUCHE_HAUT = 38;
-var TOUCHE_BAS = 40; 
-var GAUCHE = -1;
-var DROITE = 1;
-var NOMBRE_LIGNE_MAX = 14;
-var PLANCHER = 580;
-
-var NOMBRE_COLONNE = 10;
-var DIMENSION_BLOC = 40;
-var LIMITTE_GAUCHE =0;
-var LIMITTE_DROITE =LIMITTE_GAUCHE + (10 * DIMENSION_BLOC);
-var NOMBRE_BLOC_LIGNE = 3;
-
-
 //variables
 var gameOver = false ;
-var tableauBloc = new Array(10,10);
-
-//je ne suis pas sur s'il faut initialiser un array.... !!!!!!!!!!!!!!?!?!?!?!
-//mais ça bugais avant que je le fasse
-for (var i = 0;i<NOMBRE_LIGNE_MAX+1;i++){
-	tableauBloc[i]=new Array();
-	for (var j = 0;j<NOMBRE_COLONNE;j++){
-		tableauBloc[i][j]=0;
-	}
-}
-
-var tableauPlancher = new Array();
-var compteur=0;
-var direction=0;
-var rotation = false;
-var idBloc = 0;
 var blocActif = 0 ;
-var acceleration = false;
-
+var jeu = 0;
 window.onload = initialisation;
+
 function initialisation(){
 //initialisation du jeu et de ses variables
-	LIMITTE_GAUCHE =  document.getElementById("jeu").offsetLeft;	
-	console.log("Limitte gauche"+LIMITTE_GAUCHE);
-	LIMITTE_DROITE =LIMITTE_GAUCHE + (10 * DIMENSION_BLOC);
- 	for (var i=0;i<NOMBRE_COLONNE;i++){
-		tableauPlancher[i]=0;
-	}
+    
+    //initialisation du jeu
+    jeu = new Jeu();
 
-	//initialisation du premier bloc
-	blocActif = new Bloc(idBloc);
-	idBloc++;
-
-	//debut du jeu :
-	tic();
+    //initialisation du premier bloc
+    blocActif =jeu.nouveauBloc();
+    
+    //debut du jeu :
+    tic();
 }
 
 /* ********** ********** ********** ********** ********** ********** ********** */
@@ -76,91 +38,55 @@ function initialisation(){
 /* ********** ********** ********** ********** ********** ********** ********** */
 function tic(){
 //tic boucle evnementielle
-
-	compteur++;
 	if(!blocActif.actif && !gameOver){
 	/*lorsqu'un bloc as fini de descendre on l'ajoute au tableau
 	 de bloc, on verifie s'il y a des lignes qui se sont complete
 	et on ajoute un nouveau bloc 
 	*/
-		//ajouter le bloc au tableau
-		var colonne = blocActif.colonne;
-		var ligne = tableauPlancher[colonne];
-		console.log("tic() ligne");
-		tableauBloc[ligne][colonne] = blocActif;
-
-
-		// vérifier s'il y a des lignes qui se sont complété
-		for(var i=0;i<10;i++){
-			for(var j;j<10;j++){
-				var retour = validerLigne(i,j,0);			
-				//faire la même choses pour les colonnes
-			}
-		}
-		blocActif = new Bloc(idBloc);
-		idBloc++;
+	
+            //ajouter le bloc au tableau
+	    jeu.ajoutBloc();
+            
+            // vérifier s'il y a des lignes qui se sont complété
+            jeu.verifierLigne();
+	    
+            //créer un nouveau bloc
+            blocActif = jeu.nouveauBloc();
 	}
 	if(!gameOver){
-	blocActif.tic();
-	//rappel de la fonction a toutes les 30 milisecondes :
-	setTimeout(tic,30);
+	    blocActif.tic();
+
+	    //rappel de la fonction a toutes les 30 milisecondes :
+	    setTimeout(tic,30);
 	}
 	else{
 		console.log("gameover !!!!!")
-	
 		//faire une fonction qui affiche un div gameover pendand 
 		//15 secondes puis qui rapelle initialisation
 	}
 }
-
-function validerLigne(ligne,Colonne,compteur){
-/*valide pour un bloc si les suivants sur la lignes sont de la même couleur
-s'il y en as plus de 3 en ligne ils sont tous retiré. la fonction retourne le
-nombre de bloc à retirer*/
-	var id = tableauBloc[ligne][colonne].idCSS;
-	var suivante = colone+1;
-	if(tableauBloc[ligne][colonne].couleur == tableauBloc[ligne][suivante].couleur) {
-	//si le bloc suivant est de la même couleur on avance et on compte
-		compteur++;
-		var nombreARetirer = validerLigne(ligne,suivante,compteur);
-	}
-	else if(compteur < NOMBRE_BLOC_LIGNE ){
-	//si le bloc suivant n'est pas de la même couleur et qu'on en as compté
-	//moins de 3 on retourne 0 bloc à retirer 
-		return 0;
-	}
-	else{
-	//si on as compté plus de trois bloc de même couleur on incrémente le compteur	
-	//et on l'assigne aux blos à retirer
-		nombreARetirer = compteur++;
-	}
-	if(nombreARetirer > 0){
-		//on retire le div du html et l'objet du tableau
-		$("#"+id).remove();
-		tableauBloc[ligne].splice(colonne,1);
-		nombreARetirer--;
-		return nombreARetirer;
-	}
-	else{
-	 return 0;
-	}
-}
-
-
 /* ********** ********** ********** ********** ********** ********** ********** */
 				//gestion des evenements
 /* ********** ********** ********** ********** ********** ********** ********** */
 window.onkeyup = relache;
 window.onkeydown = touche;
+//constantes
+var GAUCHE = -1;
+var DROITE = 1;
+var TOUCHE_GAUCHE = 37;
+var TOUCHE_DROITE = 39;
+var TOUCHE_HAUT = 38;
+var TOUCHE_BAS = 40; 
 function touche(evenement){
-//	console.log("touche");	
-	this.code = evenement.which;
-	if(this.code == TOUCHE_GAUCHE) direction = GAUCHE ;
-	else if(this.code == TOUCHE_DROITE) direction = DROITE;
-	else if(this.code == TOUCHE_HAUT) rotation = true;
-	else if(this.code == TOUCHE_BAS) acceleration = true;
+//debug
+    console.log("touche");	
+    this.code = evenement.which;
+    if(this.code == TOUCHE_GAUCHE) blocActif.direction = GAUCHE ;
+    else if(this.code == TOUCHE_DROITE) blocActif.direction = DROITE;
+    else if(this.code == TOUCHE_HAUT) blocActif.rotation = true;
+    else if(this.code == TOUCHE_BAS) blocActif.acceleration = true;
 }
 function relache(){
-	direction=0;
-	if(acceleration)acceleration=false;
+    blocActif.direction=0;
+    if(acceleration)blocActif.acceleration=false;
 }
